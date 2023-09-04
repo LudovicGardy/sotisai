2a3efc688f521e07e6c60e6e471ed0835706b7d9
LudovicGardy <ludovic.gardy@gmail.com>
2023-09-04 19:44:22 +0200

    changed hero video

--- src/pages/Home/Hero_top_section.js
+++ src/pages/Home/Hero_top_section.js
@@ -7,12 +7,12 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video
-        // src="/videos/iStock-1310742509.mp4"
-        src="https://elasticbeanstalk-eu-north-1-643287291923.s3.eu-north-1.amazonaws.com/iStock-1310742509.mp4"
+        src="images/video/1080885575-sd.mp4"
         autoPlay
         loop
         muted
       />
+      <div className="video-overlay"></div> {/* Ajout de la div pour l'obscurcissement */}
       <div className="hero-title-container">
         <h1 className="hero-h1">Empowering Solutions Through </h1>
         <h1 className="hero-h1">Insightful Analytics</h1>
@@ -35,7 +35,6 @@ function HeroSection() {
           redirection="/data-science"
         >
           DATA SCIENCE 
-          {/* <i className="far fa-play-circle" /> */}
         </Button>
       </div>
     </div>
