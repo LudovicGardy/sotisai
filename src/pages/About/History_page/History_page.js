09e221e8b9cc96654006ecd88251b970f5dd34e6
Ludovic Gardy <ludovic.gardy@gmail.com>
2023-11-16 15:44:32 +0100

    improved about

--- src/pages/About/History_page/History_page.js
+++ src/pages/About/History_page/History_page.js
@@ -56,8 +56,9 @@ function HistoryPage() {
                     <li className={styles.myLi} style={{'--accent-color': '#6e51a8'}}>
                         <div className={styles.myDate}>June 2023</div>
                         <div className={styles.myTitle}>Founder of Sotis A.I.&#174;</div>
-                        <div className={styles.myDescr}>Founded Sotis A.I.®, a company providing data science and development services to 
-                        industries, academia, and healthcare centers.</div>
+
+                        <div className={styles.myDescr}>Sotis AI® specializes in developping advanced data engineering pipelines 
+                        and artificial intelligence solutions that transform complex data into strategic insights for businesses.</div>
                     </li>
                     {/* <li className={styles.myLi} style={{'--accent-color': '#6e51a8'}}>
                         <div className={styles.myDate}>August 2023</div>
@@ -67,66 +68,67 @@ function HistoryPage() {
                     </li> */}
                     <li className={styles.myLi} style={{'--accent-color': '#3c3c4b'}}>
                         <div className={styles.myDate}>December 2022</div>
-                        <div className={styles.myTitle}>Award RISE, CNRS</div>
-                        <div className={styles.myDescr}>Received the RISE award from CNRS Innovation, which supports startups with innovative projects.</div> 
+                        <div className={styles.myTitle}>Award RISE, CNRS Innovation</div>
+                        <div className={styles.myDescr}>I was rewarded for having developed a technological solution with strong industrial and valorization potential in the deeptech market.</div> 
                         {/* using technologies developed in CNRS labs.</div> */}
                     </li>   
                     <li className={styles.myLi} style={{'--accent-color': '#e24A68'}}>
                         <div className={styles.myDate}>Oct. 2021 - June 2023</div>
                         <div className={styles.myTitle}>Research engineer, CNRS</div>
-                        <div className={styles.myDescr}>Worked a year-and-a-half on developing Halyzia® for potential industrial partnerships and spin-off creation.</div>
+                        <div className={styles.myDescr}>Maturation and regulatory protection of the Halyzia® algorithm and software, developed during my thesis, with a view to its commercialization.</div>
                     </li>   
                     <li className={styles.myLi} style={{'--accent-color': '#6e51a8'}}>
                         <div className={styles.myDate}>November 2021</div>
                         <div className={styles.myTitle}>Award tech. pre-maturation</div>
-                        <div className={styles.myDescr}>Received the tech. pre-maturation award from the Occitanie region, which supports the commercialization 
-                        of public research results.</div>
+                        <div className={styles.myDescr}>This prize allowed my team and me to finance the technological and business development of Halyzia®, diagnostic assistance technology, aiming its regulatory marking its marketing.</div>
                     </li>   
                     <li className={styles.myLi} style={{'--accent-color': '#3c3c4b'}}>
-                        <div className={styles.myDate}>October 2021</div>
-                        <div className={styles.myTitle}>PhD end</div>
-                        <div className={styles.myDescr}>Developed Halyzia®, a tool for automatic detection of pathological brain oscillations in epilepsy. 
-                        The <a target="_blank" href="https://www.theses.fr/2021TOU30190">thesis</a> manuscript and 
+                        <div className={styles.myDate}>Oct. 2018 - Oct. 2021</div>
+                        <div className={styles.myTitle}>PhD Neuroscience, AI specialization</div>
+                        <div className={styles.myDescr}>Developed Halyzia®, an innovative deeptech solution for the automatic detection of pathological intracerebral oscillations in epilepsy using artificial neural networks. 
+                        My <a target="_blank" href="https://www.theses.fr/2021TOU30190">thesis manuscript</a> and 
                         the <a target="_blank" href="https://data.inpi.fr/brevets/FR3128111">patent</a> are available online.</div>
                     </li>    
                     <li className={styles.myLi} style={{'--accent-color': '#e24A68'}}>
-                        <div className={styles.myDate}>March 2020</div>
-                        <div className={styles.myTitle}>Award Innovation</div>
-                        <div className={styles.myDescr}>Recognized in the France 2030 plan's Innovation competition, aimed at supporting innovative 
-                        technology startup projects.</div>
-                    </li>   
-                    <li className={styles.myLi} style={{'--accent-color': '#6e51a8'}}>
-                        <div className={styles.myDate}>October 2019</div>
-                        <div className={styles.myTitle}>Award Doc. d'Occitanie</div>
-                        <div className={styles.myDescr}>Received the Doc. d'Occitanie award, designed to support innovative thesis projects 
-                        with high potential for economic spin-offs.</div>
-                    </li>
-                    <li className={styles.myLi} style={{'--accent-color': '#3c3c4b'}}>
-                        <div className={styles.myDate}>October 2018</div>
-                        <div className={styles.myTitle}>PhD start</div>
-                        <div className={styles.myDescr}>Commenced a PhD on the automatic detection of pathological intracerebral oscillations in 
-                        epilepsy using artificial neural networks
-                        at <a target="_blank" href="https://www.univ-tlse3.fr/">UPS</a>
-                        , <a target="_blank" href="https://cerco.cnrs.fr/"> CNRS</a>,  
-                        and <a target="_blank" href="https://www.enac.fr/fr">ENAC</a>.</div>
-                    </li>
-                    <li className={styles.myLi} style={{'--accent-color': '#e24A68'}}>
-                        <div className={styles.myDate}>2018-2021</div>
+                        <div className={styles.myDate}>Oct. 2018- Oct. 2021</div>
                         <div className={styles.myTitle}>Part-time teacher</div>
                         <div className={styles.myDescr}>Taught statistics at the Paul Sabatier University of Toulouse, 
                         and neurophysiology to orthoptists at the Medical School of Toulouse.</div>
                     </li>
                     <li className={styles.myLi} style={{'--accent-color': '#6e51a8'}}>
-                        <div className={styles.myDate}>2017-2018</div>
-                        <div className={styles.myTitle}>Project engineer, CNRS</div>
-                        <div className={styles.myDescr}>Worked on a year-long project, focusing on the analysis of EEG rhythms and neuronal activity. 
-                        Developed signal processing algorithms and machine learning procedures.</div>
+                        <div className={styles.myDate}>March 2020</div>
+                        <div className={styles.myTitle}>Award Innovation</div>
+                        <div className={styles.myDescr}>The Prime Minister and BPI France rewarded me for the development of Halyzia® during my research work, a cutting-edge innovative technology supported by France Relance 2030.</div>    
+                    </li>   
+                    <li className={styles.myLi} style={{'--accent-color': '#3c3c4b'}}>
+                        <div className={styles.myDate}>October 2019</div>
+                        <div className={styles.myTitle}>Award Science-to-Business</div>
+                        <div className={styles.myDescr}>Received the Doc. d'Occitanie award, designed to support innovative research projects with high marketing potential.</div>
+                    </li>
+                    <li className={styles.myLi} style={{'--accent-color': '#e24A68'}}>
+                        <div className={styles.myDate}>Dec. 2017 - Oct. 2018</div>
+                        <div className={styles.myTitle}>Big Data processing Engineer</div>
+                        <div className={styles.myDescr}>Designed experimental protocols for studying human behavior | Developed data analysis tools | Managed massive data: billions of entries, terabytes, multi-dimensionality, infrastructure design and creation.</div>
+                    </li>
+                    <li className={styles.myLi} style={{'--accent-color': '#6e51a8'}}>
+                        <div className={styles.myDate}>June 2017 - June 2018</div>
+                        <div className={styles.myTitle}>Scientific Coordinator</div>
+                        <div className={styles.myDescr}>For the Brain Awareness Week national event. Design of themes and selection of more than 50 speakers | Negotiation with nearly 40 local partners | Supervision of several dozen volunteers |  34 workshops and conferences.</div>
                     </li>
                     <li className={styles.myLi} style={{'--accent-color': '#3c3c4b'}}>
-                        <div className={styles.myDate}>2015-2017</div>
-                        <div className={styles.myTitle}>Master's degree</div>
-                        <div className={styles.myDescr}>Specialized in neuroscience at the Biology and Health Department of the University of Toulouse. 
-                        Research projects involved data collection, machine learning, and biostatistics.</div>
+                        <div className={styles.myDate}>June 2017 - Dec. 2017</div>
+                        <div className={styles.myTitle}>Data analysis Engineer assistant</div>
+                        <div className={styles.myDescr}>Scaling and structuring data processing pipelines, databases and algorithms to study the cognitive decline over 20K parameters in 250 pré-Alzheimer patients. Subtle changes observed up to 12 years before diagnosis.</div>
+                    </li>
+                    <li className={styles.myLi} style={{'--accent-color': '#e24A68'}}>
+                        <div className={styles.myDate}>Nov. 2015- June 2017</div>
+                        <div className={styles.myTitle}>Intern in AI and Big Data</div>
+                        <div className={styles.myDescr}>Collection and cleaning of large quantities of medical data | Structuring of databases according to SQL standards to prepare for massive exploitation and the use of machine learning algorithms | Statistical analyses.</div>
+                    </li>
+                    <li className={styles.myLi} style={{'--accent-color': '#6e51a8'}}>
+                        <div className={styles.myDate}>Sept. 2015 - June 2017</div>
+                        <div className={styles.myTitle}>MD Neurosciences, AI specialization </div>
+                        <div className={styles.myDescr}>Paul Sabatier University and National School Aviation</div>
                     </li>
                 </ul>
             </div>
